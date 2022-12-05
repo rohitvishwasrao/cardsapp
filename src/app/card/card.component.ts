507ac05b0867354e4b63f0c74a7e643d5a28c8a4
rohitvishwasrao <rohitvishwasrao23@gmail.com>
2022-12-05 11:20:19 +0530

    first commit

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  @Input() vada = '';
+
+  @Input() Username = '';
+
+  @Input() imageUrl = '';
+
+  @Input() content = '';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
